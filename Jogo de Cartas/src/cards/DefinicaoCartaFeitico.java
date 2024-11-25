0a92b787572a4d41545cb2a63bdf2564b5333816
Pedro AV <97904358+PedroArvina@users.noreply.github.com>
2024-11-25 17:47:30 -0300

    Update DefinicaoCartaFeitico.java

--- Jogo de Cartas/src/cards/DefinicaoCartaFeitico.java
+++ Jogo de Cartas/src/cards/DefinicaoCartaFeitico.java
@@ -13,11 +13,12 @@ public class DefinicaoCartaFeitico {
     }
 
     private void definirFeiticos() {
-        feiticosDefinidos.put(45, new Feitico("Bola de Energia", 4, "Causa 2 de dano a todos do tabuleiro", "Causa 2 de dano a geral", 0, 6, "src/fotos/46.png") {});
-        feiticosDefinidos.put(46, new Feitico("Bola de Energia", 4, "Causa 2 de dano a todos do tabuleiro", "Causa 2 de dano a geral", 0, 6, "src/fotos/47.png") {});
-        feiticosDefinidos.put(47, new Feitico("Bola de Energia", 4, "Causa 2 de dano a todos do tabuleiro", "Causa 2 de dano a geral", 0, 6, "src/fotos/48.png") {});
-        feiticosDefinidos.put(48, new Feitico("Proteção Arcana", 3, "Aumenta 3 de vida a todos", "Aumenta a vida geral em 3", 4, 0, "src/fotos/49.png") {});
-        feiticosDefinidos.put(49, new Feitico("Armagedom", 7, "Mata todos do Tabuleiro", "Kill @e", 0, 0, "src/fotos/50.png") {});
+        // Atualizando os IDs para a faixa correta de 46 a 49
+        feiticosDefinidos.put(46, new Feitico("Bola de Energia", 4, "Causa 2 de dano a todos do tabuleiro", "Causa 2 de dano a geral", 1, 1, "src/fotos/46.png", 46));
+        feiticosDefinidos.put(47, new Feitico("Bola de Energia", 4, "Causa 2 de dano a todos do tabuleiro", "Causa 2 de dano a geral", 1, 1, "src/fotos/47.png", 47));
+        feiticosDefinidos.put(48, new Feitico("Bola de Energia", 4, "Causa 2 de dano a todos do tabuleiro", "Causa 2 de dano a geral", 1, 1, "src/fotos/48.png", 48));
+        feiticosDefinidos.put(49, new Feitico("Proteção Arcana", 3, "Aumenta 3 de vida a todos", "Aumenta a vida geral em 3", 1, 1, "src/fotos/49.png", 49));
+        feiticosDefinidos.put(50, new Feitico("Armagedom", 7, "Mata todos do Tabuleiro", "Kill @e", 1, 1, "src/fotos/50.png", 50));
     }
 
     public Feitico buscarFeitico(int id) {
